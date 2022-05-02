fbf16d2ac9249b16814bc0f8b99a201447442e31
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2022-05-02 02:35:16 -0400

    first upload

--- sources/populate_test.js
+++ sources/populate_test.js
@@ -1,126 +1,114 @@
+let parsed = [];
+const imgUrl = "https://eddiedohyunkim.github.io/ansonli/sources/populate_test.json"
+	fetch(imgUrl)
+	.then(function(response){return response.json();})
+	.then(function(json){parsed=json})
+
+let home_btn = document.getElementById('nav_title');
+home_btn.addEventListener('click', ContClose, false);
 
 // nav bar number button 1
 let num_1_btn = document.getElementById('nav_num_1');
-num_1_btn.addEventListener('click', initiate, false);
+num_1_btn.addEventListener('click', vidPopUp, false);
 num_1_btn.parameter = '1';
 
 // nav bar number button 2
 let num_2_btn = document.getElementById('nav_num_2');
-num_2_btn.addEventListener('click', initiate, false);
+num_2_btn.addEventListener('click', imgPopUp, false);
 num_2_btn.parameter = '2';
 
 // nav bar number button 3
 let num_3_btn = document.getElementById('nav_num_3');
-num_3_btn.addEventListener('click', initiate, false);
+num_3_btn.addEventListener('click', imgPopUp, false);
 num_3_btn.parameter = '3';
 
 // nav bar number button 4
 let num_4_btn = document.getElementById('nav_num_4');
-num_4_btn.addEventListener('click', initiate, false);
+num_4_btn.addEventListener('click', imgPopUp, false);
 num_4_btn.parameter = '4';
 
 
-// function initiate(e){
-// 	window.alert(e.currentTarget.parameter);
-// }
 
+function ContClose(){
+	let getImgCont = document.getElementById('imageContainer');
+	if(Boolean(getImgCont)){ getImgCont.remove();}
+	let getVidCont = document.getElementById('videoContainer');
+	if(Boolean(getVidCont)){ getVidCont.remove();}
+}
 
-function initiate(e){
+function vidPopUp(e){
 	let parameter = e.currentTarget.parameter
-	// disable(parameter);
-	closeBtn();
-	makeImgCont();
-	setColumn();
-	populate(parameter);
+	makeVidCont();
+	populateVid(parameter);
 }
 
-function disable(number){
-	let numbers = [1,2,3,4];
-	numbers.splice(number-1, 1);
-	for(const element of numbers){
-		document.getElementById(`nav_num_${element}`).removeEventListener('click', initiate, false);
-	}
+function imgPopUp(e){
+	let parameter = e.currentTarget.parameter
+	makeImgCont();
+	populateImg(parameter);
 }
-// function del(){
-// 	document.getElementById('imageContainer').remove();
-// }
-function closeBtn(){
-	// document.getElementById('nav_cont').style.display = 'none';
+
+function makeVidCont(){
+	ContClose()
+	let createCont = document.createElement('div');
+	createCont.setAttribute('id', 'videoContainer');
+	document.body.appendChild(createCont);
+	let videoHolder = document.createElement('div');
+	videoHolder.setAttribute('id', 'videoHolder');
+	document.getElementById('videoContainer').appendChild(videoHolder);
 }
 
 function makeImgCont(){
-
-	var get = document.getElementById('imageContainer');
-	if(Boolean(get)){ get.remove();}
-	var createCont = document.createElement('div');
+	ContClose()
+	let createCont = document.createElement('div');
 	createCont.setAttribute('id', 'imageContainer');
 	document.body.appendChild(createCont);
-	
 }
 
-window.addEventListener("resize", setColumn);
-var WorH;
-var size;
-var columnNumber;
-var categoryGap;
-
-function setColumn(){
-	var width = document.body.clientWidth;
-	console.log(width);
-	if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-		function detectOrientation(){
-			if(typeof window.onorientationchange != 'undefined'){
-				if(window.orientation==0){
-					columnNumber = 2;
-				}else if(window.orientation==90 || window.orientation==-90){
-					columnNumber = 3;
-				} // fi
-			}
-		} 
-		detectOrientation();
-		window.onorientationchange = detectOrientation;
-	}else if(/iPad/i.test(navigator.userAgent)){
-		function detectOrientation(){
-			if(typeof window.onorientationchange != 'undefined'){
-				if(window.orientation==0){ 
-					columnNumber = 4;
-				}else if(window.orientation==90 || window.orientation==-90){
-					columnNumber = 4;
-				} // fi
-			}
-		} 
-		detectOrientation();
-		window.onorientationchange = detectOrientation;
-	}else if(width<600){
-		columnNumber = 2;
-	}else if(width<900){
-		columnNumber = 3;
-	}else if(width<1200){
-		columnNumber = 4;
-	}else{
-		columnNumber = 5;
-	}
-	document.getElementById('imageContainer').style.gridTemplateColumns = `repeat(${columnNumber},1fr)`;
+function populateVid(number){
+	let video = document.createElement('video');
+	video.setAttribute('id', 'video');
+	video.src = 'https://eddiedohyunkim.github.io/ansonli/assets/1_Collection_Video/In_Situ-_Collection_Intro.mp4';
+	video.autoplay = true;
+	video.muted = true;
+	document.getElementById('videoHolder').appendChild(video);
+	video.addEventListener('ended',ContClose,false);
 }
 
-function populate(number){
+function populateImg(number){
+	console.log(number);
 	let format;
-	if(number==2){
-		format='jpg';
-	}else{
-		format='jpeg';
-	}
-	for(var i=0; i<21; i+=1){
-		var box = document.createElement('div');
+	let source = parsed[number-2].source
+	// console.log(parsed[number-2].id);
+	for(let i=0; i<source.length; i+=1){
+		let box = document.createElement('div');
 		box.setAttribute('id', 'box'+i);
 		box.setAttribute('class', 'boxes');
 		document.getElementById('imageContainer').appendChild(box);
 	}
-	for(var i=0; i<21; i+=1){
-		var image = document.createElement('img');
-		image.setAttribute('src', `assets/image/${number}/test_c${number}_${i}.${format}`);
-		// image.setAttribute('id', 'image'+i);
+	for(let i=0; i<source.length; i+=1){
+		let image = document.createElement('img');
+		image.setAttribute('src', source[i]);
 		image.setAttribute('class', 'popImages');
-		document.getElementById('box'+i).appendChild(image);	
+		document.getElementById('box'+i).appendChild(image);
 	}
+
+	// if(number==2){
+	// 	format='jpg';
+	// }else{
+	// 	format='jpeg';
+	// }
+	// for(let i=0; i<21; i+=1){
+	// 	let box = document.createElement('div');
+	// 	box.setAttribute('id', 'box'+i);
+	// 	box.setAttribute('class', 'boxes');
+	// 	document.getElementById('imageContainer').appendChild(box);
+	// }
+	// for(let i=0; i<21; i+=1){
+	// 	let image = document.createElement('img');
+	// 	image.setAttribute('src', `assets/image/${number}/test_c${number}_${i}.${format}`);
+	// 	// image.setAttribute('id', 'image'+i);
+	// 	image.setAttribute('class', 'popImages');
+	// 	document.getElementById('box'+i).appendChild(image);	
+	// }
 }
